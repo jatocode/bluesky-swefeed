61e0d135e67335e226f3a386dbf2d940f56515e0
Tobias <tobias.jansson@gmail.com>
2023-07-30 12:56:26 +0200

    publish settings

--- scripts/publishFeedGen.ts
+++ scripts/publishFeedGen.ts
@@ -8,24 +8,25 @@ const run = async () => {
 
   // YOUR bluesky handle
   // Ex: user.bsky.social
-  const handle = ''
+  const handle = 'tobiasjansson.dev'
 
-  // YOUR bluesky password, or preferably an App Password (found in your client settings)
-  // Ex: abcd-1234-efgh-5678
-  const password = ''
+  if (!process.env.FEEDGEN_PUBLISH_APP_PASSWORD) {
+    throw new Error('Please provide an app password in the .env file')
+  }
+  const password = process.env.FEEDGEN_PUBLISH_APP_PASSWORD
 
   // A short name for the record that will show in urls
   // Lowercase with no spaces.
   // Ex: whats-hot
-  const recordName = ''
+  const recordName = 'sweskyfeed'
 
   // A display name for your feed
   // Ex: What's Hot
-  const displayName = ''
+  const displayName = 'Bluesky Swedes'
 
   // (Optional) A description of your feed
   // Ex: Top trending content from the whole network
-  const description = ''
+  const description = 'Finds all posts with content-lang set to sv'
 
   // (Optional) The path to an image to be used as your feed's avatar
   // Ex: ~/path/to/avatar.jpeg
